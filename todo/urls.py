a6d454a204ddf9eedaa668ea1347b8cae1c958e9
ravwanovv05 <samidilloravshanov14@gmail.com>
2023-12-02 18:25:05 +0500

    change

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from todo.views import AddTodoAPIView, UpdateDestroyTodoGenericAPIView, OwnerTodoGenericAPIView, TodoInfoAPIView
+from todo.views import AddTodoAPIView, UpdateDestroyTodoGenericAPIView, OwnerTodoGenericAPIView, TodoInfoGenericAPIView
 
 urlpatterns = [
     path('add-todo', AddTodoAPIView.as_view(), name='add-todo'),
     path('update-delete/<int:pk>', UpdateDestroyTodoGenericAPIView.as_view(), name='update-delete'),
     path('owner-todo', OwnerTodoGenericAPIView.as_view(),  name='owner-todo'),
-    path('todo-info/<int:pk>', TodoInfoAPIView.as_view(), name='todo-info'),
+    path('todo-info/<int:pk>', TodoInfoGenericAPIView.as_view(), name='todo-info'),
 ]
 
--- todo/views.py
+++ todo/views.py
@@ -10,26 +10,6 @@ from .serializers import TodoSerializer, UpdateDestroyTodoSerializer
 User = get_user_model()
 
 
-# for user
-# class AddTodoAPIView(APIView):
-#     permission_classes = (IsAuthenticated,)
-#
-#     def post(self, request):
-#         text = request.POST.get('text')
-#         owner = request.user.id
-#
-#         try:
-#             owner = User.objects.get(pk=owner)
-#             todo = Todo.objects.create(
-#                 text=text,
-#                 owner=owner,
-#             )
-#             todo.save()
-#         except Exception as e:
-#             return Response({'success': False, 'message': str(e)})
-#         return Response({'success': True, 'message': 'Successfully added todo!'})
-
-
 # generic
 class AddTodoAPIView(GenericAPIView):
     permission_classes = (IsAuthenticated,)
@@ -68,30 +48,6 @@ class UpdateDestroyTodoGenericAPIView(GenericAPIView):
         return Response(status=204)
 
 
-# class OwnerTodoAPIView(APIView):
-#     permission_classes = (IsAuthenticated,)
-#
-#     def get(self, request):
-#         owner = request.user.id
-#         query_todo = Todo.objects.filter(owner=owner)
-#         todo_data = []
-#
-#         try:
-#             for todo in query_todo:
-#                 todo_dict = {
-#                     'id': todo.id,
-#                     'text': todo.text,
-#                     'expires_at': todo.expires_at,
-#                     'owner': owner,
-#                 }
-#                 todo_data.append(todo_dict)
-#         except Exception as e:
-#             return Response({'success': False, 'message': str(e)})
-#         return Response(todo_data)
-
-
-# generic
-
 class OwnerTodoGenericAPIView(GenericAPIView):
     permission_classes = (IsAuthenticated,)
     serializer_class = TodoSerializer
@@ -102,23 +58,14 @@ class OwnerTodoGenericAPIView(GenericAPIView):
         return Response(serializer_todo.data)
 
 
-class TodoInfoAPIView(APIView):
+class TodoInfoGenericAPIView(GenericAPIView):
     permission_classes = (IsAuthenticated,)
+    serializer_class = TodoSerializer
 
     def get(self, request, pk):
         owner = request.user.id
         todo = Todo.objects.get(Q(owner=owner) & Q(id=pk))
-        # try:
-        #     todo_dict = {
-        #         'id': todo.id,
-        #         'text': todo.text,
-        #         'expires_at': todo.expires_at,
-        #         'owner': owner,
-        #     }
-        # except Exception as e:
-        #     return Response({'success': False, 'message': str(e)})
-        # return Response(todo_dict)
-        todo_serializer = TodoSerializer(todo)
+        todo_serializer = self.get_serializer(todo)
         return Response(todo_serializer.data)
 
 
